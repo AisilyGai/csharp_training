61e7fdd845ad1c23c0f3222d44be08aa4d55cf29
Aisily <aisgaim@yandex.ru>
2020-05-03 22:31:19 +0300

    9.3

---
+++ mantis-tests/appmanager/RegistrationHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using OpenQA.Selenium;
+
+namespace mantis_tests
+{
+    public class RegistrationHelper : HelperBase
+    {
+        public RegistrationHelper(ApplicationManager manager) : base(manager) { }
+
+        public void Register(AccountData account)
+        {
+            OpenMainPage();
+            OpenRegistrationForm();
+            FillRegistrationForm(account);
+            SubmitRegistration();
+            String URL = GetConfirmationUrl(account);
+            FillPasswordForm(URL);
+            SubmitPasswordForm();
+        }
+
+        private string GetConfirmationUrl(AccountData account)
+        {
+            String message = manager.Mail.GetLastMail(account);
+            Match match = Regex.Match(message, @"http://\S*");
+            return match.Value;
+        }
+
+        private void FillPasswordForm(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SubmitPasswordForm()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OpenRegistrationForm()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Зарегистрировать новую учётную запись")).Click();
+        }
+
+        private void SubmitRegistration()
+        {
+            driver.FindElement(By.XPath("//input[2]")).Click();
+        }
+
+        private void FillRegistrationForm(AccountData account)
+        {
+            driver.FindElement(By.Name("username")).SendKeys(account.Name);
+            driver.FindElement(By.Name("email")).SendKeys(account.Email);
+        }
+
+        private void OpenMainPage()
+        {
+            manager.Driver.Url = "http://localhost/mantisbt-2.24.0/login_page.php";
+        }
+    }
+}
